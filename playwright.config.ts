e3aee7209cbb33aae023d1ab01af410280f730f8
vinhtester <duongvinh1706@gmail.com>
2024-03-25 23:00:05 +0700

    Integrate reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
   // reporter: "allure-playwright",
-  reporter: [['allure-playwright'], ['line']],
+  reporter: [['allure-playwright',{outputFolder: 'test-results'}], ['line']],
   globalSetup: require.resolve('./utils/global-setup'),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
